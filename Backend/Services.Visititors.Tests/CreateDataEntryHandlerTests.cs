b20facbb8ee13271dac566d514df28192b1bd722
Billy Mumby <billy.mumby@outlook.com>
2020-01-07 13:47:53 +0000

    Added some unit tests for some of the visitor command handlers classes.

---
+++ Backend/Services.Visititors.Tests/CreateDataEntryHandlerTests.cs
@@ -0,0 +1,90 @@
+using System;
+using System.Linq;
+using System.Threading.Tasks;
+using Moq;
+using NUnit.Framework;
+using Services.Common.Exceptions;
+using Services.RabbitMq.Interfaces.Messaging;
+using Services.RabbitMq.Messages;
+using Services.Tests.Mocks;
+using Services.Visitors.Commands;
+using Services.Visitors.Domain;
+using Services.Visitors.Events;
+using Services.Visitors.Factories;
+using Services.Visitors.Handlers.Command;
+using Services.Visitors.Names;
+using Services.Visitors.Repositorys;
+
+namespace Services.Visitors.Tests
+{
+    public class CreateDataEntryHandlerTests
+    {
+
+        private Mock<IRequestInfo> _requestInfo;
+        private Mock<IServiceBusMessagePublisher> _publisher;
+        private Mock<IDataSpecificationFactory> _factory;
+        private Mock<IDataSpecificationRepository> _repository;
+
+
+        [SetUp]
+        public void Setup()
+        {
+            _requestInfo = new Mock<IRequestInfo>();
+            _publisher = new Mock<IServiceBusMessagePublisher>();
+            _factory = new Mock<IDataSpecificationFactory>();
+            _repository = new Mock<IDataSpecificationRepository>();
+
+            _factory.Setup(o => o.Create(It.IsAny<string>(), It.IsAny<int>(), It.IsAny<string>(), It.IsAny<string>(),
+                It.IsAny<Guid>())).Returns(new Mock<IDataSpecification>().Object);
+        }
+
+        [Test]
+        public async Task HandleAsync_Always_GetsNextOrderNumber()
+        {
+            //Arrange
+            var sut = CreateSut();
+
+            //Act
+            await sut.HandleAsync(new CreateDataEntry("Test", "message", "Required",
+                Guid.NewGuid()), _requestInfo.Object);
+
+            //Assert
+            _repository.Verify(o => o.GetNextOrderNumberAsync(It.IsAny<Guid>()));
+        }
+
+        [Test]
+        public async Task HandleAsync_Always_PublishesDataSpecificationRejectedWhenValidationFails()
+        {
+            //Arrange
+            var sut = CreateSut();
+            _factory.Setup(o => o.Create(It.IsAny<string>(), It.IsAny<int>(), It.IsAny<string>(), It.IsAny<string>(),
+                It.IsAny<Guid>())).Throws(new VmsException("code", "reason"));
+
+            //Act
+            await sut.HandleAsync(new CreateDataEntry("Test", "message", "Required",
+                Guid.NewGuid()), _requestInfo.Object);
+
+            //Assert
+            _publisher.Verify(o => o.PublishEvent(It.Is<DataSpecificationRejected>(x => x.Code == "code" && x.Reason == "reason"), _requestInfo.Object));
+        }
+
+        [Test]
+        public async Task HandleAsync_Always_CreatesSpecificationIfValid()
+        {
+            //Arrange
+            var sut = CreateSut();
+
+            //Act
+            await sut.HandleAsync(new CreateDataEntry("Test", "message", "Required",
+                Guid.NewGuid()), _requestInfo.Object);
+
+
+            //Assert
+            _repository.Verify(o => o.AddAsync(It.IsAny<IDataSpecification>()));
+            _publisher.Verify(o => o.PublishEvent(It.IsAny<DataSpecificationCreated>(), _requestInfo.Object));
+        }
+
+        public CreateDataEntryHandler CreateSut() => new CreateDataEntryHandler(LoggerMock.CreateVms<CreateDataEntryHandler>(), _publisher.Object, _factory.Object,
+            _repository.Object);
+    }
+}
---
+++ Backend/Services.Visititors.Tests/DeprecateDataEntryHandlerTests.cs
@@ -0,0 +1,144 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Moq;
+using NUnit.Framework;
+using Services.RabbitMq.Interfaces.Messaging;
+using Services.RabbitMq.Messages;
+using Services.Tests.Mocks;
+using Services.Visitors.Commands;
+using Services.Visitors.Domain;
+using Services.Visitors.Events;
+using Services.Visitors.Handlers.Command;
+using Services.Visitors.Repositorys;
+
+namespace Services.Visitors.Tests
+{
+    public class DeprecateDataEntryHandlerTests
+    {
+
+        private Mock<IRequestInfo> _requestInfo;
+        private Mock<IServiceBusMessagePublisher> _publisher;
+        private Mock<IDataSpecificationRepository> _repository;
+        private Guid _targetId = Guid.NewGuid();
+        private Mock<IDataSpecification> _target;
+
+        private IEnumerable<IDataSpecification> GetEntries(int count, int target)
+        {
+            for (int i = 0; i < count; i++)
+            {
+                var specMock = new Mock<IDataSpecification>();
+                if (i == target)
+                {
+                    _target.Setup(o => o.Order).Returns(i + 1);
+                    yield return _target.Object;
+                }
+                specMock.SetupGet(o => o.Order).Returns(i + 1);
+                yield return specMock.Object;
+            }
+            
+        }
+
+
+        [SetUp]
+        public void Setup()
+        {
+            _requestInfo = new Mock<IRequestInfo>();
+            _publisher = new Mock<IServiceBusMessagePublisher>();
+            _repository = new Mock<IDataSpecificationRepository>();
+            _target = new Mock<IDataSpecification>();
+            _target.Setup(o => o.Id).Returns(_targetId);
+        }
+
+        [Test]
+        public async Task HandleAsync_Always_PublishesDeprecationRejectedWhenNotDataForBusiness()
+        {
+            //Arrange
+            var sut = CreateSut();
+
+            //Act
+            await sut.HandleAsync(new DeprecateDataEntry(Guid.NewGuid(), Guid.NewGuid()), _requestInfo.Object);
+
+            //Assert
+            _publisher.Verify(o => o.PublishEvent(It.Is<DataEntryDeprecationRejected>(x => x.Code == Codes.InvalidBusinessId), _requestInfo.Object));
+        }
+
+        [Test]
+        public async Task HandleAsync_Always_PublishesDeprecationRejectedWhenDataSpecNotFound()
+        {
+            //Arrange
+            var sut = CreateSut();
+
+            _repository.Setup(o => o.GetEntriesAsync(It.IsAny<Guid>())).Returns(Task.FromResult(GetEntries(10, -1)));
+
+            //Act
+            await sut.HandleAsync(new DeprecateDataEntry(Guid.NewGuid(), Guid.NewGuid()), _requestInfo.Object);
+
+            //Assert
+            _publisher.Verify(o => o.PublishEvent(It.Is<DataEntryDeprecationRejected>(x => x.Code == Codes.InvalidId), _requestInfo.Object));
+        }
+
+        [Test]
+        public async Task HandleAsync_Always_DeprecatesAndUpdatesTargetSpec()
+        {
+            //Arrange
+            var sut = CreateSut();
+
+            _repository.Setup(o => o.GetEntriesAsync(It.IsAny<Guid>())).Returns(Task.FromResult(GetEntries(10, 4)));
+
+            //Act
+            await sut.HandleAsync(new DeprecateDataEntry(_targetId, Guid.NewGuid()), _requestInfo.Object);
+
+            //Assert
+            _target.Verify(o => o.Deprecate());
+            _repository.Verify(o => o.UpdateAsync(_target.Object));
+        }
+
+        [TestCase(20, 5)]
+        [TestCase(2, 1)]
+        [TestCase(27, 8)]
+        public async Task HandleAsync_Always_ReOrderRemainingSpecs(int collectionSize, int targetLocation)    
+        {
+            //Arrange
+            var sut = CreateSut();
+
+            var entries = GetEntries(collectionSize, targetLocation);
+
+            _repository.Setup(o => o.GetEntriesAsync(It.IsAny<Guid>())).Returns(Task.FromResult(entries));
+
+
+            //Act
+            await sut.HandleAsync(new DeprecateDataEntry(_targetId, Guid.NewGuid()), _requestInfo.Object);
+
+            //Assert
+            for (int i = 0; i < collectionSize - 1; i++)
+            {
+                _repository.Verify(o => o.UpdateAsync(It.IsAny<IDataSpecification>()));
+            }
+        }
+
+        [TestCase(20, 5)]
+        [TestCase(2, 1)]
+        [TestCase(27, 8)]
+        public async Task HandleAsync_Always_PublhisesDeprecatedWhenComplete(int collectionSize, int targetLocation)
+        {
+            //Arrange   
+            var sut = CreateSut();
+
+            var entries = GetEntries(collectionSize, targetLocation);
+
+            _repository.Setup(o => o.GetEntriesAsync(It.IsAny<Guid>())).Returns(Task.FromResult(entries));
+
+
+            //Act
+            await sut.HandleAsync(new DeprecateDataEntry(_targetId, Guid.NewGuid()), _requestInfo.Object);
+
+            //Assert
+            _publisher.Verify(o => o.PublishEvent(It.IsAny<DataEntryDeprecated>(), _requestInfo.Object));
+        }
+
+        public DeprecateDataEntryHandler CreateSut() => new DeprecateDataEntryHandler(LoggerMock.CreateVms<DeprecateDataEntryHandler>(), _publisher.Object, _repository.Object);  
+    }
+}
